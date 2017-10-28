a6b7f2afe1683b9c79033c8fcf242b590dcd5a3a
Jesse Li <jesse@sandboxx.us>
2017-10-28 15:41:18 -0400

    Added skeleton ProviderServices activity and fragment

--- app/src/main/java/com/servpal/android/activities/ProviderServicesActivity.java
+++ app/src/main/java/com/servpal/android/activities/ProviderServicesActivity.java
@@ -1,15 +1,59 @@
 package com.servpal.android.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.servpal.android.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ProviderServicesActivity extends AppCompatActivity {
+
+    @BindView(R.id.backdrop)
+    ImageView backdrop;
+
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+
+    // some kind of local data
+    // ProviderServicesList that has Profile and List<Services>?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_provider_services);
+        setContentView(R.layout.activity_provider_services);    // TODO: layout has hella work left
+        ButterKnife.bind(this);
+
+        // network call for provider services
+        // populate image
+        // populate title
+        // generate Tabs
+        // create fragments with ViewPager
+    }
+
+    private class ProviderServicesPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ProviderServicesPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) { // fragments accept bundle args to instantiate?
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
