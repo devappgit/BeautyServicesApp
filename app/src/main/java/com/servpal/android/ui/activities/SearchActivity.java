2c8d9ce673ed7ddc8fdbd8b44381a62bf08ac9bd
Jesse Li <jesse@sandboxx.us>
2017-11-18 17:45:17 -0500

    Can't use collections.emptyList

--- app/src/main/java/com/servpal/android/ui/activities/SearchActivity.java
+++ app/src/main/java/com/servpal/android/ui/activities/SearchActivity.java
@@ -2,34 +2,33 @@ package com.servpal.android.ui.activities;
 
 import android.os.Bundle;
 
-import com.servpal.android.adapter.SearchResultAdapter;
 import com.servpal.android.api.NetworkCallback;
 import com.servpal.android.api.ServpalHttpClient;
-import com.servpal.android.model.Professional;
+import com.servpal.android.experimental.adapter.SearchPagingAdapter;
 import com.servpal.android.model.SearchResult;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import timber.log.Timber;
 
 public class SearchActivity extends AbsRecyclerActivity {
 
-    private SearchResultAdapter adapter;
+    private SearchPagingAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        adapter = new SearchResultAdapter(Collections.emptyList());
+        adapter = new SearchPagingAdapter();
+        adapter.setResults(new ArrayList<>());
         getRecycler().setAdapter(adapter);
 
         callProfessionalsSearch(null)
                 .enqueue(new NetworkCallback<SearchResult>() {
                     @Override
                     protected void onSuccess(SearchResult response) {
-                        adapter = new SearchResultAdapter(response.getProfessionals());
-                        getRecycler().setAdapter(adapter);
+                        adapter.addAll(response.getProfessionals());
                     }
 
                     @Override
@@ -46,6 +45,7 @@ public class SearchActivity extends AbsRecyclerActivity {
     @Override
     protected void onRefresh() {
         Timber.d("Refresh triggered");
+
         getRefreshLayout().setRefreshing(false);
     }
 
