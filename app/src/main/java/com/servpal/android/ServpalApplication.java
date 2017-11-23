e7395d92940bd6507b9d3324b958ce276b3581bd
Jesse Li <jesse@sandboxx.us>
2017-11-22 20:30:58 -0500

    Added Fabric and Crashlytics

--- app/src/main/java/com/servpal/android/ServpalApplication.java
+++ app/src/main/java/com/servpal/android/ServpalApplication.java
@@ -2,10 +2,13 @@ package com.servpal.android;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 import com.servpal.android.model.Session;
 import com.servpal.android.utils.ProductionTree;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class ServpalApplication extends Application {
@@ -13,6 +16,11 @@ public class ServpalApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.Builder builder = new Fabric.Builder(this)
+                .kits(
+                        new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build()
+                );
+        Fabric.with(builder.build());
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
