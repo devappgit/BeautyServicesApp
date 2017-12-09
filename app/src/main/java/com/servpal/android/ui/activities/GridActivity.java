3cb9aa3037ac3b117f197d4f00ad67ffe6beab0a
Jesse Li <jesse@sandboxx.us>
2017-12-09 13:35:26 -0500

    Added grid skeletons and string resources

---
+++ app/src/main/java/com/servpal/android/ui/activities/GridActivity.java
@@ -0,0 +1,14 @@
+package com.servpal.android.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
