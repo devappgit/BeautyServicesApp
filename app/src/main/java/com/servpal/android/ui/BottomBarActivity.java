d9f250bbb778b9a7db30374bb41fb62400ee91fd
Jesse Li <jesse@sandboxx.us>
2017-12-09 15:15:05 -0500

    Fragments, unswipeable viewpager, and bottom bar mostly set up now

--- app/src/main/java/com/servpal/android/ui/BottomBarActivity.java
+++ app/src/main/java/com/servpal/android/ui/BottomBarActivity.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 import com.servpal.android.R;
+import com.servpal.android.ToggleableViewPager;
+import com.servpal.android.adapter.PagingAdapter;
+import com.servpal.android.ui.fragments.DashboardFragment;
+import com.servpal.android.ui.fragments.ProfileFragment;
+import com.servpal.android.ui.fragments.SearchFragment;
+import com.servpal.android.ui.fragments.SettingsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BottomBarActivity extends AppCompatActivity {
 
-    @BindView(R.id.frame)
-    FrameLayout frame;
+    @BindView(R.id.view_pager)
+    ToggleableViewPager pager;
     @BindView(R.id.bottom_navigation)
     BottomNavigationView bottomBar;
 
@@ -26,17 +34,25 @@ public class BottomBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_bar);
         ButterKnife.bind(this);
 
+        MainPagingAdapter adapter = new MainPagingAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        pager.setPagingEnabled(false);
+
         bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()) {
                     case R.id.action_search:
+                        pager.setCurrentItem(0, false);
                         break;
                     case R.id.action_dashboard:
+                        pager.setCurrentItem(1, false);
                         break;
                     case R.id.action_profile:
+                        pager.setCurrentItem(2, false);
                         break;
                     case R.id.action_settings:
+                        pager.setCurrentItem(3, false);
                         break;
                     default:
                         break;
@@ -44,5 +60,32 @@ public class BottomBarActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    static class MainPagingAdapter extends FragmentPagerAdapter {
+
+        MainPagingAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return new SearchFragment();
+                case 1:
+                    return new DashboardFragment();
+                case 2:
+                    return new ProfileFragment();
+                case 3:
+                    return new SettingsFragment();
+            }
+            return new Fragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
     }
 }
--- app/src/main/java/com/servpal/android/ui/fragments/SettingsFragment.java
+++ app/src/main/java/com/servpal/android/ui/fragments/SettingsFragment.java
@@ -1,4 +1,25 @@
 package com.servpal.android.ui.fragments;
 
-public class SettingsFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.servpal.android.R;
+
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
